e04caaf498e31ec825594bc2b6d8de243690b9f7
tpetersen0308 <travis.e.petersen@gmail.com>
2018-03-28 13:03:47 -0700

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,37 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    msg = self.check_tickets + self.check_height
+
+    if !msg.empty?
+      msg.prepend("Sorry.")
+    else
+      self.charge_user
+      self.nauseate_user
+      self.please_user
+      self.user.save
+    end
+  end
+
+  def charge_user
+    self.user.tickets -= self.attraction.tickets
+  end
+
+  def nauseate_user
+    self.user.nausea += self.attraction.nausea_rating
+  end
+
+  def please_user
+    self.user.happiness += self.attraction.happiness_rating
+  end
+
+  def check_height
+    self.user.height < self.attraction.min_height ? " You are not tall enough to ride the #{self.attraction.name}." : ""
+  end
+
+  def check_tickets
+    self.user.tickets < self.attraction.tickets ? " You do not have enough tickets to ride the #{self.attraction.name}." : ""
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-  # write associations here
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  has_secure_password
+
+  def mood
+    self.nausea >= self.happiness ? "sad" : "happy"
+  end
 end
